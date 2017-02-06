9d369606c6785850cd03a1ec96a0a92b33009eaa
Gareth Paul Jones <gareth@garethpaul.com>
2017-02-06 13:43:02 -0800

    Initial Commit

---
+++ PlaceShapes/PlaceShapes.h
@@ -0,0 +1,19 @@
+//
+//  PlaceShapes.h
+//  PlaceShapes
+//
+//  Created by Gareth Paul Jones on 2/6/17.
+//  Copyright © 2017 Gareth Paul Jones. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PlaceShapes.
+FOUNDATION_EXPORT double PlaceShapesVersionNumber;
+
+//! Project version string for PlaceShapes.
+FOUNDATION_EXPORT const unsigned char PlaceShapesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PlaceShapes/PublicHeader.h>
+
+
